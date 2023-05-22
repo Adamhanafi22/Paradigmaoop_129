807913f3d2c077a774c51636a6157dd898b8529e
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-05-22 18:57:12 +0700

    methoud

--- mainIA/Ibu.h
+++ mainIA/Ibu.h
@@ -1,1 +1,23 @@
+
 #pragma once
+#ifndef IBU_H
+#define IBU_H
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) : nama(pNama) {
+		cout << "ibu\"" << nama << "\" ada \n";
+	}
+	~ibu() {
+		cout << "ibu \"" << nama << "\" tidak ada\n";
+	}
+	 
+};
+#endif // !IBU_H
+
