347cfdd99c9179ebcb59793fa650dbb4b32b3391
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-05-22 20:41:26 +0700

    methoud pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -34,3 +34,13 @@ public:
 	void tambahpasien(pasien*);
 	void cetakpasien();
 }; 
+void pasien::tambahDokter(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
