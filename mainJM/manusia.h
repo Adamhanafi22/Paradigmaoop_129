ce5134244ef6b77a2143b7da5a3c2b01e0671ef9
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-05-22 19:54:20 +0700

    class manusia

--- mainJM/manusia.h
+++ mainJM/manusia.h
@@ -1,1 +1,23 @@
 #pragma once
+#ifndef MANUSIA_H
+#define MANUSIA_H
+#include <iostream>
+#include <string> 
+using namespace std;
+
+class manusia {
+public:
+	string name;
+	jantung varjantung;
+
+	manusia(string pName)
+		: name(pName) {
+		cout << name << "hidup\n";
+	}
+	~manusia() {
+		cout << name << "mati\n";
+	}
+};
+
+#endif // !MANUSIA_H
+
